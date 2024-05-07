d4742a84491c2cdaa1a657fe595d9d53f599be7b
skyran1278 <skyran1278@gmail.com>
2024-05-07 18:33:13 +0800

    feat: experimenting with cqrs. part 2

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -3,6 +3,7 @@ import { AlarmFactory } from '../domain/factories/alarm.factory';
 import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmsService } from './alarms.service';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
 import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
 
 @Module({
@@ -12,6 +13,7 @@ import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
     AlarmFactory,
     CreateAlarmCommandHandler,
     GetAlarmsQueryHandler,
+    AlarmCreatedEventHandler,
   ],
 })
 export class AlarmsModule {
