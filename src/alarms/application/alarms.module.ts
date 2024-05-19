97df66d616e6f10b6c4fa7f942b3f578804d62ec
skyran1278 <skyran1278@gmail.com>
2024-05-19 08:06:44 +0800

    feat: understanding sagas

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -4,9 +4,12 @@ import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmsService } from './alarms.service';
 import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.command-handler';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { NotifyFacilitySupervisorCommandHandler } from './commands/notify-facility-supervisor.command-handler';
 import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledged.event-handler';
 import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
 import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
+import { CascadingAlarmsSaga } from './sagas/cascading-alarms.saga';
+import { UnacknowledgedAlarmsSaga } from './sagas/unacknowledged-alarms.saga';
 
 @Module({
   controllers: [AlarmsController],
@@ -18,6 +21,9 @@ import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
     AlarmCreatedEventHandler,
     AcknowledgeAlarmCommandHandler,
     AlarmAcknowledgedEventHandler,
+    CascadingAlarmsSaga,
+    NotifyFacilitySupervisorCommandHandler,
+    UnacknowledgedAlarmsSaga,
   ],
 })
 export class AlarmsModule {
